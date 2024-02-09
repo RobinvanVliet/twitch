f6a19109d7fce37c9f3954170480084804625b8a
RobinvanVliet <r.vanvliet97@gmail.com>
2024-02-09 13:41:04 +0100

    Add ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from "@clerk/themes";
+
 import { Inter } from "next/font/google";
 
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,10 +20,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+    <ClerkProvider appearance={{baseTheme: dark}}>
+        <html lang="en">
+          <body className={inter.className}>
+            <ThemeProvider
+              attribute="class"
+              forcedTheme="dark"
+              storageKey="gamehub-theme"
+            >
+              {children}
+            </ThemeProvider>
+          </body>
+        </html>
     </ClerkProvider>
   );
 }
